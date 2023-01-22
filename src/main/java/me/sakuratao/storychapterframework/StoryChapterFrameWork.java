8657df1105c63cb2e65465d7b122733c56ae59fc
SakuraTao <2585257181@qq.com>
2023-01-22 20:36:11 +0800

    阶段性保存

--- src/main/java/me/sakuratao/storychapterframework/StoryChapterFrameWork.java
+++ src/main/java/me/sakuratao/storychapterframework/StoryChapterFrameWork.java
@@ -4,27 +4,17 @@ import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import me.sakuratao.storychapterframework.api.SCFWAPI;
 import me.sakuratao.storychapterframework.api.SCFWAPIProvider;
-import me.sakuratao.storychapterframework.data.DataAccessor;
-import me.sakuratao.storychapterframework.data.Story.StoryData;
-import me.sakuratao.storychapterframework.data.player.PlayerData;
-import me.sakuratao.storychapterframework.handler.HandlerManager;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import top.jingwenmc.spigotpie.common.instance.PieComponent;
+import top.jingwenmc.spigotpie.common.instance.Wire;
 
-import java.util.UUID;
-import java.util.function.Consumer;
-
+@PieComponent
 public final class StoryChapterFrameWork extends JavaPlugin {
 
     public static final ProtocolManager api = ProtocolLibrary.getProtocolManager();
-    public static StoryChapterFrameWork staticInstance;
-    public StoryChapterFrameWork instance;
-    public HandlerManager handlerManager = new HandlerManager();
 
     @Override
     public void onLoad() {
-        staticInstance = this;
-        instance = this;
     }
 
     @Override
