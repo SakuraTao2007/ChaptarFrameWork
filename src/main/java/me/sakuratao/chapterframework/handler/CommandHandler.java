580fea44c88135f0afab39bfff16a69fb0d36d2b
SakuraTao <2585257181@qq.com>
2023-02-05 12:14:28 +0800

    改进

--- src/main/java/me/sakuratao/chapterframework/handler/CommandHandler.java
+++ src/main/java/me/sakuratao/chapterframework/handler/CommandHandler.java
@@ -15,14 +15,14 @@ public class CommandHandler implements SchedulerHelper, MessageHelper {
     @Wire
     ChapterFramework storyChapterFrameWork;
 
-    @PieCommand(value = "scfw",permission = "scfw.command", bungeeCord = false, spigot = true)
+    @PieCommand(value = "chapter",permission = "cfw.command", bungeeCord = false, spigot = true)
     public void onMainCommand(CommandSender sender){
         sender.sendMessage(ChatColor.translate("&8&m---»--*-------------------------------------*--«---"));
         sender.sendMessage(ChatColor.translate("&r"));
-        sender.sendMessage(ChatColor.translate("&r  &f&lStoryChapterFrameWork &8| &fVer: &7" + storyChapterFrameWork.getDescription().getVersion()));
+        sender.sendMessage(ChatColor.translate("&r  &f&lChapterFrameWork &8| &fVer: &7" + storyChapterFrameWork.getDescription().getVersion()));
         sender.sendMessage(ChatColor.translate("&r  &fAuthor: &7" + storyChapterFrameWork.getDescription().getAuthors()));
         sender.sendMessage(ChatColor.translate("&r"));
-        sender.sendMessage(ChatColor.translate("&r   &7* &f/scfw storyChapter &8| &f查看故事章节指令树"));
+        sender.sendMessage(ChatColor.translate("&r   &7* &f/chapter &8| &f查看故事章节指令树"));
         sender.sendMessage(ChatColor.translate("&r"));
         sender.sendMessage(ChatColor.translate("&8&m---»--*-------------------------------------*--«---"));
     }
