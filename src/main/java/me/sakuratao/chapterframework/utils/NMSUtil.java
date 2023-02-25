54668ef6fb36e92709a3bb7b67a319b4915df420
SakuraTao <2585257181@qq.com>
2023-02-25 17:35:57 +0800

    对支持版本判断的改进

--- src/main/java/me/sakuratao/chapterframework/utils/NMSUtil.java
+++ src/main/java/me/sakuratao/chapterframework/utils/NMSUtil.java
@@ -1,6 +1,7 @@
 package me.sakuratao.chapterframework.utils;
 
 import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
 
 public class NMSUtil {
     private static String version;
@@ -16,12 +17,14 @@ public class NMSUtil {
     private NMSUtil() {
     }
 
+    @NotNull
     public static String getNMSVersion() {
         return Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
     }
 
     public static boolean isSupported(){
-        if (version == null)
+        String[] split = version.split("_");
+        if (Integer.parseInt(split[1]) < 19)
             return false;
         return version.startsWith("v1_19_R2");
     }
